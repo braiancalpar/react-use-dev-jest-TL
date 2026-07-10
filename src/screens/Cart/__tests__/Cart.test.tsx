9a3dfe31bcb311337696ea76c9189b44b4a49b1e
braiancalpar <braian.santos@calpar.com.br>
2026-07-10 11:22:41 -0300

    feat: adicionar mock de fetch e navegação para testes no componente Cart

---
+++ src/screens/Cart/__tests__/Cart.test.tsx
@@ -0,0 +1,24 @@
+export const mockFetch = jest.fn((url: string) => {
+  if (url.includes("/categories")) {
+    return Promise.resolve({
+      json: () => Promise.resolve({}),
+    });
+  }
+  if (url.includes("/products")) {
+    return Promise.resolve({
+      json: () => Promise.resolve({}),
+    });
+  }
+  return Promise.resolve({
+    json: () => Promise.resolve({}),
+  });
+});
+
+globalThis.fetch = mockFetch as jest.Mock;
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useNavigate: () => mockNavigate,
+}));
